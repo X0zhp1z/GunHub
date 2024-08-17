e21074039da00b3b2a00cfae1be036f4f5272b94
X0 <106098316+X0zhp1z@users.noreply.github.com>
2024-08-17 14:13:47 -0300

    Delete Site Armas directory

--- Site Armas/assets/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.arma').forEach(item => {
-    item.addEventListener('click', event => {
-        item.querySelectorAll('p').forEach(p => p.classList.toggle('hidden'));
-    });
-});
